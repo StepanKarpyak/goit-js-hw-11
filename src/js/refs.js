ed17143fdf7c2ff25d85128081b0add34856ae60
StepanKarpyak <stepan_karpyak@ukr.net>
2021-11-28 22:55:24 +0200

    corection

--- src/js/refs.js
+++
@@ -1,6 +0,0 @@
-export const refs = {
-  form: document.querySelector('#search-form'),
-  searchInput: document.querySelector('[name="searchQuery"]'),
-  button: document.querySelector('.load-more'),
-  gallery: document.querySelector('.gallery'),
-};
